e79f78ca830bd9f930b3d75741951ef4a25e5808
RedJohn260 <damir.k93@gmail.com>
2023-01-24 13:47:45 +0100

    added GiveMoney script

---
+++ GiveMoney/Givemoney.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+using GTA;
+
+namespace Givemoney
+{
+    public class Givemoney : Script
+    {
+        public Givemoney()
+        {
+            KeyDown += Givemoney_KeyDown;
+            Game.DisplayText("Givemoney mod loaded");
+        }
+
+        private void Givemoney_KeyDown(object sender, GTA.KeyEventArgs e)
+        {
+            if (e.Key == Keys.F11)
+            {
+                GiveMoney();
+            }
+        }
+
+        private void GiveMoney()
+        {
+            if (Player.isPlaying)
+            {
+                int money = Player.Money;
+                int amount = 1000;
+                money = money + amount;
+                Player.Money= money;
+                Helper.ShowSubtitle("You got $ " + amount.ToString());
+            }
+            else
+            {
+                Helper.ShowSubtitle("Player not playing");
+            }
+        }
+    }
+}
---
+++ GiveMoney/Helper.cs
@@ -0,0 +1,76 @@
+﻿using GTA.Native;
+using GTA;
+using System;
+using System.Drawing;
+
+namespace Givemoney
+{
+    public sealed class Helper
+    {
+        public static void SetCarColor(Vehicle veh, int col1, int col2)
+        {
+            Function.Call("CHANGE_CAR_COLOUR", new Parameter[]
+            {
+                veh,
+                col1,
+                col2
+            });
+        }
+
+        public static Point GetCarColor(Vehicle veh)
+        {
+            Pointer pointer = new Pointer(typeof(int));
+            Pointer pointer2 = new Pointer(typeof(int));
+            Function.Call("GET_CAR_COLOURS", new Parameter[]
+            {
+                veh,
+                pointer,
+                pointer2
+            });
+            return new Point(pointer, pointer2);
+        }
+
+        public static void SetExtraCarColor(Vehicle veh, int col1, int col2)
+        {
+            Function.Call("SET_EXTRA_CAR_COLOURS", new Parameter[]
+            {
+                veh,
+                col1,
+                col2
+            });
+        }
+
+        public static Point GetExtraCarColor(Vehicle veh)
+        {
+            Pointer pointer = new Pointer(typeof(int));
+            Pointer pointer2 = new Pointer(typeof(int));
+            Function.Call("GET_EXTRA_CAR_COLOURS", new Parameter[]
+            {
+                veh,
+                pointer,
+                pointer2
+            });
+            return new Point(pointer, pointer2);
+        }
+
+        public static int RandomColor()
+        {
+            int min = 0;
+            int max = 136;
+            Random random = new Random();
+            int result = random.Next(min, max);
+            return result;
+        }
+
+        public static void ShowSubtitle(string text, int duration = 4000)
+        {
+            Function.Call("PRINT_STRING_WITH_LITERAL_STRING_NOW", new Parameter[]
+            {
+                "STRING",
+                text,
+                duration,
+                1
+            });
+        }
+    }
+}
